a7da6910bf3f3988081dc466e38cb0d171393dd8
jokowncode <958031210@qq.com>
2025-08-06 19:16:10 +0800

    New: BattleDataGenerator

---
+++ Assets/Editor/BattleDataGeneratorCustomEditor.cs
@@ -0,0 +1,15 @@
+﻿
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(BattleDataGenerator))]
+public class BattleDataGeneratorCustomEditor : Editor {
+    public override void OnInspectorGUI(){
+        base.OnInspectorGUI();
+        BattleDataGenerator generator = (BattleDataGenerator)target;
+        if (GUILayout.Button("生成")){
+            generator.Generate();
+        }
+    }
+}
+
---
+++ Assets/Scripts/Tools/BattleDataGenerator.cs
@@ -0,0 +1,34 @@
+﻿
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class BattleDataGenerator : MonoBehaviour{
+
+    [SerializeField] private Transform EnemyParent;
+    [SerializeField] private string AssetName;
+
+    public void Generate(){
+        if (!EnemyParent || EnemyParent.childCount == 0) return;
+        BattleData data = ScriptableObject.CreateInstance<BattleData>();
+        List<EnemyDepartmentData> enemyDepartmentDatas = new List<EnemyDepartmentData>();
+        foreach (Transform enemyTrans in EnemyParent){
+            if (enemyTrans.TryGetComponent(out Enemy enemy)) {
+                Enemy prefab = PrefabUtility.GetCorrespondingObjectFromSource(enemy);
+                enemyDepartmentDatas.Add(new EnemyDepartmentData{
+                    Position = enemyTrans.position,
+                    EnemyPrefab = prefab
+                });
+            }
+        }
+        data.EnemiesInBattle = enemyDepartmentDatas;
+        string path = $"Assets/ScriptableObjects/BattleData/{AssetName}.asset";
+        AssetDatabase.CreateAsset(data, path);
+        AssetDatabase.SaveAssets();
+        AssetDatabase.Refresh();
+        Debug.Log("Success Generate Battle Data");
+    }
+
+}
+
+
