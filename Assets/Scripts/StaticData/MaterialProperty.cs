3fc371d02f5c7c71f3cb28036fee14a8b37330d7
jokowncode <958031210@qq.com>
2025-08-06 20:27:13 +0800

    New: Flashing and Dissolve VFX

---
+++ Assets/Scripts/StaticData/MaterialProperty.cs
@@ -0,0 +1,8 @@
+﻿
+using UnityEngine;
+
+public static class MaterialProperty {
+    public static readonly int Flashing = Shader.PropertyToID("_Flashing");
+    public static readonly int Dissolve = Shader.PropertyToID("_Dissolve");
+}
+
