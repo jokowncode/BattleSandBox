4dfc6ac446c3898d138e05e207069a626161c782
jokowncode <958031210@qq.com>
2025-08-07 21:36:24 +0800

    New: Footstep Sfx

--- Assets/Scripts/Skill/Caster/FurthestTargetSkillCaster.cs
+++ Assets/Scripts/Skill/Caster/FurthestTargetSkillCaster.cs
@@ -4,19 +4,24 @@ using UnityEngine;
 [RequireComponent(typeof(Buff))]
 public class FurthestTargetSkillCaster : SkillCaster {
     
-    public BuffData BuffData;
+    [SerializeField] private BuffData BuffData;
     
-    protected override void Cast(Transform _) {
-        ParticleSystem ps = Instantiate(SkillStartParticle, OwnedFighter.transform.position, Quaternion.identity);
-        ps.Play();
-        Destroy(ps.gameObject, ps.main.duration + ps.main.startLifetime.constantMax);
-        Fighter fighter = BattleManager.Instance.FindFurthestEnemyTarget(OwnedFighter.transform.position);
+    protected override void Cast(Transform _){
+        if (SkillStartParticle){
+            ParticleSystem ps = Instantiate(SkillStartParticle, OwnedFighter.transform.position, Quaternion.identity);
+            ps.Play();
+            Destroy(ps.gameObject, ps.main.duration + ps.main.startLifetime.constantMax);
+        }
         
+        Fighter fighter = BattleManager.Instance.FindFurthestEnemyTarget(OwnedFighter.transform.position);
         OwnedFighter.transform.position = fighter.transform.position + Vector3.right * OwnedFighter.AttackRadius;
-        ps = Instantiate(SkillStartParticle, fighter.transform.position, Quaternion.identity);
-        ps.Play();
-        Destroy(ps.gameObject, ps.main.duration + ps.main.startLifetime.constantMax);
-        // TODO: Buff
-        this.GetComponent<Buff>().AddBuff(this.OwnedFighter,this.OwnedFighter,BuffData);
+
+        if (SkillStartParticle){
+            ParticleSystem ps = Instantiate(SkillStartParticle, fighter.transform.position, Quaternion.identity);
+            ps.Play();
+            Destroy(ps.gameObject, ps.main.duration + ps.main.startLifetime.constantMax);
+        }
+        
+        if(BuffData) this.GetComponent<Buff>().AddBuff(this.OwnedFighter,this.OwnedFighter,BuffData);
     }
 }
