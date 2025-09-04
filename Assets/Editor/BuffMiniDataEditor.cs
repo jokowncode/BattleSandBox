e3a6b49d1e7285c2f7d606307457036dcace0a01
Jokown <958031210@qq.com>
2025-09-04 21:10:33 +0800

    New: BuffMiniData Editor

---
+++ Assets/Editor/BuffMiniDataEditor.cs
@@ -0,0 +1,46 @@
+﻿
+using System;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(BuffMiniData))]
+public class BuffMiniDataEditor : Editor {
+
+    private SerializedObject Obj;
+    private BuffMiniData Target;
+
+    private GUIStyle TitleStyle;
+
+    private void OnEnable() {
+        Obj = new SerializedObject(target);
+        TitleStyle = new GUIStyle {
+            fontStyle = FontStyle.Bold, 
+            normal = new GUIStyleState { textColor = Color.white }
+        };
+    }
+
+    public override void OnInspectorGUI() {
+        // base.OnInspectorGUI();
+        Target = (BuffMiniData)target;
+        
+        GUILayout.Label("Property Change", this.TitleStyle);
+        Target.ModifyWay = (PropertyModifyWay)EditorGUILayout.EnumPopup("ModifyWay", Target.ModifyWay);
+        
+        if (Target.ModifyWay == PropertyModifyWay.Percentage) {
+            EditorGUILayout.PropertyField(Obj.FindProperty("Ref"));
+            EditorGUILayout.PropertyField(Obj.FindProperty("PropertyRef"));
+            EditorGUILayout.PropertyField(Obj.FindProperty("CasterProperty"));
+            EditorGUILayout.PropertyField(Obj.FindProperty("TargetRefProperty"));
+        }
+        
+        EditorGUILayout.PropertyField(Obj.FindProperty("TargetUpdateProperty"));
+        EditorGUILayout.PropertyField(Obj.FindProperty("ChangedValue"));
+        EditorGUILayout.PropertyField(Obj.FindProperty("IsChangeProperty"));
+        
+        GUILayout.Space(20);
+        GUILayout.Label("Particle", this.TitleStyle);
+        EditorGUILayout.PropertyField(Obj.FindProperty("IsDestroyImmediate"));
+        EditorGUILayout.PropertyField(Obj.FindProperty("DestroyDelay"));
+        EditorGUILayout.PropertyField(Obj.FindProperty("EffectParticlePrefab"));
+    }
+}
--- Assets/Scripts/ScriptableObjects/Buff/BuffMiniData.cs
+++ Assets/Scripts/ScriptableObjects/Buff/BuffMiniData.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 
 [CreateAssetMenu(fileName = "NewMiniBuff", menuName = "PhantomSpirit/Buff/Buff Mini Data")]
 public class BuffMiniData : ScriptableObject {
-    [Header("Property Change")]
     public BuffRef Ref;
     public PropertyRef PropertyRef;
     public FighterProperty CasterProperty;
@@ -15,7 +14,6 @@ public class BuffMiniData : ScriptableObject {
     public float ChangedValue;
     public bool IsChangeProperty = true; // For Health
 
-    [Header("Particle")] 
     public bool IsDestroyImmediate = true;
     public float DestroyDelay = 0.5f;
     public GameObject EffectParticlePrefab;
