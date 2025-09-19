85f1254dc490f079460ccd8ca77d00a330123e02
Jokown <958031210@qq.com>
2025-09-19 19:58:57 +0800

    New: Hero Arsene

---
+++ Assets/Scripts/Skill/Effect/SuckBloodSkillEffect.cs
@@ -0,0 +1,13 @@
+﻿
+public class SuckBloodSkillEffect : DirectDamageSkillEffect {
+    protected override void Apply(Fighter influenceFighter, EffectData effectData) {
+        base.Apply(influenceFighter, effectData);
+        if (this.Delivery.Caster.TryGetComponent(out Fighter fighter)) {
+            fighter.BeHealed(new EffectData {
+                Value = effectData.Value,
+                NotShowParticle = false
+            });
+        }
+    }
+}
+
