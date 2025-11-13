e528552d407d2f2ba90dc018294bc48f3341cc81
Jokown <958031210@qq.com>
2025-11-13 15:06:09 +0800

    Update: Dialog
    1. BGM Change (Fade)
    2. Volume
    3. Background Character Portrait

--- Assets/Scripts/Dialog/Node/DialogNode.cs
+++ Assets/Scripts/Dialog/Node/DialogNode.cs
@@ -8,13 +8,19 @@ public class DialogNode : Node {
     [Input] public Node PreNode;
     
     public Sprite Background;
+    public Sprite[] BackgroundCharacterPortraits;
     public bool NotBackground;
     public bool BackgroundIsFadeIn;
+    
     public AudioClip DialogAudio;
+    public AudioClip DialogBGM;
+    public float CharacterAudioVolume = 1.0f;
+    public float BGMVolume = 1.0f;
+    
     [TextArea] public string DialogText;
     public string CharacterName;
     public Sprite CharacterPortrait;
-
+    
     public string BeforeDialogInvokeAction;
     public string AfterDialogInvokeAction;
     
--- Assets/Scripts/Dialog/Node/StartNode.cs
+++ Assets/Scripts/Dialog/Node/StartNode.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 using XNode;
 
 public class StartNode : Node {
-    public AudioClip DialogBGM;
+    
     [Output] public Node NextDialog;
 
     public override object GetValue(NodePort port) {
