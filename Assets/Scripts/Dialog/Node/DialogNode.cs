e8005e8c6af79c249d25511f21d5f1ba887e9399
= <958031210@qq.com>
2026-06-26 11:50:23 +0800

    移除对话同步相关代码

--- Assets/Scripts/Dialog/Node/DialogNode.cs
+++ Assets/Scripts/Dialog/Node/DialogNode.cs
@@ -18,7 +18,7 @@ public struct OptionData {
     public OptionEndingFlagsData[] EndingFlagsDatas;
 }
 
-public class DialogNode : Node, ISerializationCallbackReceiver {
+public class DialogNode : Node {
 
     [Input] public Node PreNode;
 
@@ -57,24 +57,12 @@ public class DialogNode : Node, ISerializationCallbackReceiver {
     [Header("Action")]
     public string BeforeDialogInvokeAction;
     public string AfterDialogInvokeAction;
-    
-    [FormerlySerializedAs("Options")]
-    [SerializeField, HideInInspector]
-    private string[] _legacyOptions;
 
     [Output(dynamicPortList = true)] public OptionData[] Options;
     [Output] public Node NextDialog;
     
     public override object GetValue(NodePort port) {
         return null;
-    }
-
-    public void OnBeforeSerialize() { }
-    public void OnAfterDeserialize() {
-        if (_legacyOptions != null && _legacyOptions.Length > 0) {
-            Options = _legacyOptions.Select(s => new OptionData { OptionContent = s }).ToArray();
-            _legacyOptions = null;
-        }
     }
 }
 
